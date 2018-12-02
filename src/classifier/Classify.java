26738d8af50d299b6a7abe03172baff162c2e45b
Ivan Boatwright <ijboatwright@gmail.com>
2018-12-02 16:28:37 -0500

    Discarding the old NaiveBayes branch and starting over.

--- src/classifier/Classify.java
+++ src/classifier/Classify.java
@@ -152,9 +152,9 @@ public class Classify implements Runnable {
         // TODO Refactor all of this away to appropriate places.
         if (algorithm.toLowerCase().equals("nb")) {
             NaiveBayes nb = new NaiveBayes();
-            nb.train(wrangledTrainMessages);
 
-//        int spamCount = tokenizedMessages.stream().filter(t ->  t.FILE_NAME.startsWith("s")).mapToInt(t -> 1).sum();
+            wrangledTrainMessages.forEach(nb::train);
+            wrangledTestMessages.forEach(nb::test);
         }
     }
 
--- src/classifier/NaiveBayes.java
+++ src/classifier/NaiveBayes.java
@@ -22,20 +22,59 @@ public class NaiveBayes {
 
   private final String MODEL_SAVE_DIR = "./NB_models";
 
-//  private class Entry <T> implements Comparable<Entry<T>>{
-//    public T token;
-//    public Double probability;
-//    public Entry(T token, Double p){
-//      this.token=token;
-//      probability=p;
-//    }
-//    public int compareTo(Entry<T> e){
-//      // if this is larger returns > 0
-//      // if equal returns 0
-//      // if this is less than e returns < 0
-//      return (int) (e.probability - this.probability);
-//    }
-//  }
+  // specify the level of metadata to add
+  // level 0 - no tagging
+  // level 2 - append labels to tokens
+  private int tagging = 0;
+private class Token <T> implements Comparable<Token<T>>{
+  T token; // token symbol
+  String activeLabel;
+
+  // String is a label of whatever you feel like counting
+  HashMap<String, Integer> count; // <label, frequency_count>
+  HashMap<String, Double> probability; // <descriptive_id, calculated_probability>
+
+  public Token(T token) {
+    this.token = token;
+    this.activeLabel = "spam"; // defaulting to spam for no good reason
+    this.count = new HashMap<>();
+    this.probability = new HashMap<>();
+  }
+
+  // Increment the counter for specified label
+  void increment(String label){
+    count.put(label, count.getOrDefault(label,0)+1);
+  }
+
+  // return the count or 0 if there is no entry in the counts table
+  int getCount(String label){
+    return count.getOrDefault(label, 0);
+  }
+
+  // inserts the given value under the given label
+  void updateProbability(String label, Double p){
+    probability.put(label, p);
+  }
+
+  // returns the value associated with the label or 0.0 if the label is not in the table
+  Double getProbabiltiy(String label){
+    return probability.getOrDefault(label, 0.0);
+  }
+
+  String setActiveLabel(String label){
+    this.activeLabel = label;
+    return activeLabel;
+  }
+
+  // be sure to set the activeLabel before comparing
+  public int compareTo(Token<T> t){
+      // if this is larger returns > 0
+      // if equal returns 0
+      // if this is less than t returns < 0
+    return (int) (t.probability.getOrDefault(activeLabel,0.0) - this.probability.getOrDefault(activeLabel,0.0));
+  }
+
+}
 
   // default constructor. initializes to an untrained model
   NaiveBayes() {
@@ -148,38 +187,52 @@ public class NaiveBayes {
   }
 
 
-  // Take the messages from classifier.Classify. Stream through the messages twice,
-  //  once to process spam, second time for ham.
-  void train(List<TokenizedMessage> messages) {
+  // Processes the tokens in a message and updates the proper tables for future use.
+  void train(TokenizedMessage message) {
 //    1)
 //      1.1)
-//      1.2) increment the token count in the respective mapCount
+//      1.2)
 //      1.3) update the frequency value in the frequencyMap
 //    2) save the state of this nb
-    String label;
-    for (TokenizedMessage msg: messages) {
-      label = (msg.isSpam())? "spam" : "ham";
-      HashSet<String> tknSet = new HashSet<>();
-      tknSet.addAll(msg.getSubjectTokens());
+    String label = (message.isSpam())? "spam" : "ham";
 
-      // increment this label's message counter
-      if (label.equals("ham")){
-        incHamMsgCount();
-      } else {
-        incSpamMsgCount();
-      }
-
-      for (String tk: tknSet){
-        learn("##subject##"+tk, label);
-      }
-      for (String tk: tknSet){
-        learn("##body##"+tk, label);
-      }
+    // increment this label's message counter
+    if (label.equals("ham")){
+      incHamMsgCount();
+    } else {
+      incSpamMsgCount();
     }
 
+    // Learn each distinct token, if tagging is >= 2 tokens are prepended
+    // (hopefully) unique strings for subject or body
+    if(tagging == 0) {
+      message.getAllTokens().forEach(t->learn(t,label));
+    } else if(tagging >=2) {
+      HashSet<String> tokenSet = new HashSet<>(message.getSubjectTokens());
+      tokenSet.forEach(t->learn("##subject##" + t,label));
 
-    System.out.println("breakpoint");
+      tokenSet = new HashSet<>(message.getBodyTokens());
+      tokenSet.forEach(t->learn("##body##" + t,label));
+    }
+//    System.out.println("breakpoint");
   }
+
+
+  // Process a message and try to predict it's label based on the training data.
+  void test(TokenizedMessage message){
+    String label = (message.isSpam())? "spam" : "ham";
+    //Double prediction = predict(message.getAllTokens());
+
+    // DEBUG
+    //System.out.println(message.FILE_NAME+": "+prediction);
+  }
+
+  // If this were to be used in a live environment it would need to learn
+  // from newly predicted messages. But here we will only be learning training
+  // messages.
+//  void learn(String tk){
+//
+//  }
 
   void learn(String tk, String label){
     // add/increment token to the proper countsMap
@@ -197,12 +250,21 @@ public class NaiveBayes {
     setProbability(tk,tkProb);
   }
 
-  // need 3 token counts in messages (I think): spam, ham, totals
-  // can take a message as input then return a set<'token'>
-//  void accumulateTokens(ArrayList<String> tokens, String msgType){
-//    HashSet<String> tkSet = new HashSet<>();
-//    tkSet.addAll(tokens);
-//    tkSet.forEach(tk -> tokenCounts.putIfAbsent(tk, new Integer[]{1,1}));
-//  }
+  Double predict(List<String> tokens){
+    Double prediction = 0.0;
+    prediction = tokens.parallelStream()
+        .map(this::getProbability)
+        .filter(Objects::isNull)
+        .peek(System.out::println)
+        .reduce(1.0, (x,y)-> x*y);
+    return prediction;
+  }
+  //******************
+  // Helper functions
+  //******************
+
+  void printTokenReport(){
+    // TODO
+  }
 
 }
